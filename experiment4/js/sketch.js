165a4c548efeec8f851566717ab245c5b8af3c9b
Joshua Vaillancourt <jjvailla@ucsc.edu>
2024-02-05 23:17:19 -0800

    will it work in one try? find out

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -1,67 +1,58 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - Experiment 4 Images, Video, Sound Art
+// Author: Joshua Vaillancourt
+// Date: 2/5/24
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+var sinOsc, sinFilter, reverb, reverbTime, reverbDecay;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
 
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  // place our canvas, making it fit our container
+  canvasContainer = $("#canvas-container");
+  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+  canvas.parent("canvas-container");
+// resize canvas is the page is resized
+  $(window).resize(function() {
+      console.log("Resizing...");
+      resizeCanvas(canvasContainer.width(), canvasContainer.height());
+  });
+  background(0);
+	
+  sinFilter = new p5.BandPass();
+	
+  sinOsc = new p5.Oscillator('sawtooth');
+  sinOsc.amp(0);
+  sinOsc.freq(10);
+  sinOsc.start();
+  sinOsc.disconnect();
+  sinOsc.connect(sinFilter);
+	
+  reverb = new p5.Reverb();
+  reverbTime = 1;
+  reverbDecay = 0.5;
+  delay = new p5.Delay();
+  
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  fill(mouseX/4, mouseY/4, (mouseX+mouseY)/8)
+  circle(mouseX, mouseY, (mouseX+mouseY)/5);
+  let setFreq = ((mouseX+mouseY)*2);
+  sinFilter.freq(setFreq);
+  sinFilter.res(5);
+  sinOsc.pan((mouseX*2-windowWidth)/(windowWidth));
+  delay.process(sinOsc, (windowHeight-mouseY)/(windowHeight+100));
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function touchStarted(){
+  userStartAudio();
+  sinOsc.amp(1);
+}
+
+function mouseDragged(){
+  userStartAudio();
+  sinOsc.amp(1);
+}
+
+function touchEnded(){
+  sinOsc.amp(1)
 }

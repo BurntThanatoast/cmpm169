01c36872f7bb87eb8c331a4e90a43c441aed7d80
Joshua Vaillancourt <jjvailla@ucsc.edu>
2024-02-05 23:51:16 -0800

    fuck my life

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -7,7 +7,14 @@ var sinOsc, sinFilter;
 
 function setup() {
   // place our canvas, making it fit our container
-    createCanvas(800, 800);
+    canvasContainer = $("#canvas-container");
+    let canvas = createCanvas(800(), 800());
+    canvas.parent("canvas-container");
+// resize canvas is the page is resized
+    $(window).resize(function() {
+        console.log("Resizing...");
+        resizeCanvas(800(), 800());
+    });
     background(0);
 	
     sinFilter = new p5.BandPass();

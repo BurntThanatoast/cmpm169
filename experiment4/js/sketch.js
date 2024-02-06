25f5e491ff80ac233f58c3284535a61fa2af6918
Joshua Vaillancourt <jjvailla@ucsc.edu>
2024-02-05 23:47:37 -0800

    idk

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -7,14 +7,6 @@ var sinOsc, sinFilter;
 
 function setup() {
   // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-// resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
     createCanvas(800, 800);
     background(0);
 	
--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -7,61 +7,43 @@
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
     let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
     canvas.parent("canvas-container");
-    // resize canvas is the page is resized
+// resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
-}
-
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
+    noFill();
+    background(255);
+    strokeWeight(2);
+    stroke(0, 25);
+  }
+  
+  function draw() {
+    if (mouseIsPressed && mouseButton == LEFT) {
+      push();
+      translate(width / 2, height / 2);
+  
+      var circleResolution = int(map(mouseY + 100, 0, height, 2, 10));
+      var radius = mouseX - width / 2;
+      var angle = TAU / circleResolution;
+  
+      beginShape();
+      for (var i = 0; i <= circleResolution; i++) {
+        var x = cos(angle * i) * radius;
+        var y = sin(angle * i) * radius;
+        vertex(x, y);
+      }
+      endShape();
+  
+      pop();
+    }
+  }
+  
+  function keyReleased() {
+    if (keyCode == DELETE || keyCode == BACKSPACE) background(255);
+    if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  }

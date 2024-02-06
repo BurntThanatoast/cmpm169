5779f8d3e75edc927a8306df6543e091a4dd8f52
Joshua Vaillancourt <jjvailla@ucsc.edu>
2024-02-06 00:13:50 -0800

    fucking

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -7,43 +7,25 @@
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
+var sinOsc, sinFilter;
+
+
 function setup() {
+    // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
     let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
     canvas.parent("canvas-container");
-// resize canvas is the page is resized
+    // resize canvas is the page is resized
     $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
+      console.log("Resizing...");
+      resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    noFill();
-    background(255);
-    strokeWeight(2);
-    stroke(0, 25);
-  }
+    
+    background(0);
   
-  function draw() {
-    if (mouseIsPressed && mouseButton == LEFT) {
-      push();
-      translate(width / 2, height / 2);
-  
-      var circleResolution = int(map(mouseY + 100, 0, height, 2, 10));
-      var radius = mouseX - width / 2;
-      var angle = TAU / circleResolution;
-  
-      beginShape();
-      for (var i = 0; i <= circleResolution; i++) {
-        var x = cos(angle * i) * radius;
-        var y = sin(angle * i) * radius;
-        vertex(x, y);
-      }
-      endShape();
-  
-      pop();
-    }
-  }
-  
-  function keyReleased() {
-    if (keyCode == DELETE || keyCode == BACKSPACE) background(255);
-    if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
-  }
+}
+
+function draw() {
+    fill(mouseX/4, mouseY/4, (mouseX+mouseY)/8)
+    circle(mouseX, mouseY, (mouseX+mouseY)/5);
+}

6bc47cd9856d6125e81bc94cc8dfe3cf4f0065f8
Joshua Vaillancourt <jjvailla@ucsc.edu>
2024-02-05 23:37:04 -0800

    idk

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -7,52 +7,52 @@ var sinOsc, sinFilter, reverb, reverbTime, reverbDecay;
 
 function setup() {
   // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
+    canvasContainer = $("#canvas-container");
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    canvas.parent("canvas-container");
 // resize canvas is the page is resized
-  $(window).resize(function() {
-      console.log("Resizing...");
-      resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  });
-  background(0);
+    $(window).resize(function() {
+        console.log("Resizing...");
+        resizeCanvas(canvasContainer.width(), canvasContainer.height());
+    });
+    background(0);
 	
-  sinFilter = new p5.BandPass();
+    sinFilter = new p5.BandPass();
 	
-  sinOsc = new p5.Oscillator('sawtooth');
-  sinOsc.amp(0);
-  sinOsc.freq(10);
-  sinOsc.start();
-  sinOsc.disconnect();
-  sinOsc.connect(sinFilter);
+    sinOsc = new p5.Oscillator('sawtooth');
+    sinOsc.amp(0);
+    sinOsc.freq(10);
+    sinOsc.start();
+    sinOsc.disconnect();
+    sinOsc.connect(sinFilter);
 	
-  reverb = new p5.Reverb();
-  reverbTime = 1;
-  reverbDecay = 0.5;
-  delay = new p5.Delay();
+    reverb = new p5.Reverb();
+    reverbTime = 1;
+    reverbDecay = 0.5;
+    delay = new p5.Delay();
   
 }
 
 function draw() {
-  fill(mouseX/4, mouseY/4, (mouseX+mouseY)/8)
-  circle(mouseX, mouseY, (mouseX+mouseY)/5);
-  let setFreq = ((mouseX+mouseY)*2);
-  sinFilter.freq(setFreq);
-  sinFilter.res(5);
-  sinOsc.pan((mouseX*2-windowWidth)/(windowWidth));
-  delay.process(sinOsc, (windowHeight-mouseY)/(windowHeight+100));
+    fill(mouseX/4, mouseY/4, (mouseX+mouseY)/8)
+    circle(mouseX, mouseY, (mouseX+mouseY)/5);
+    let setFreq = ((mouseX+mouseY)*2);
+    sinFilter.freq(setFreq);
+    sinFilter.res(5);
+    sinOsc.pan((mouseX*2-windowWidth)/(windowWidth));
+    delay.process(sinOsc, (windowHeight-mouseY)/(windowHeight+100));
 }
 
 function mouseIsPressed(){
-  userStartAudio();
-  sinOsc.amp(1);
+    userStartAudio();
+    sinOsc.amp(1);
 }
 
 function mouseDragged(){
-  userStartAudio();
-  sinOsc.amp(1);
+    userStartAudio();
+    sinOsc.amp(1);
 }
 
 function mouseReleased(){
-  sinOsc.amp(0)
+    sinOsc.amp(0)
 }

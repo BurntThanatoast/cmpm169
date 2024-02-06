c281c2a31c3eb63fd7b21a9d7ada3c68e4d638c5
Joshua Vaillancourt <jjvailla@ucsc.edu>
2024-02-06 00:01:20 -0800

    bruh

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -8,12 +8,12 @@ var sinOsc, sinFilter;
 function setup() {
   // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(800, 800);
     canvas.parent("canvas-container");
 // resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
+        resizeCanvas(800, 800);
     });
     
     background(0);

74b2522d20f1ccfbafb16cb9a0c28ddfbe406f8e
Joshua Vaillancourt <jjvailla@ucsc.edu>
2024-02-13 03:29:42 -0800

    3d project 1st push

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -1,31 +1,102 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-var sinOsc, sinFilter;
-
+// sketch.js - Experiment 5 - 3D Graphics
+// Author: Joshua Vaillancourt
+// Date: 2/12/24
 
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-      console.log("Resizing...");
-      resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    
-    background(0);
-  
+  // place our canvas, making it fit our container
+  canvasContainer = $("#canvas-container");
+  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
+  canvas.parent("canvas-container");
+  // resize canvas is the page is resized
+  $(window).resize(function() {
+    console.log("Resizing...");
+    resizeCanvas(canvasContainer.width(), canvasContainer.height());
+  });
+  //createCanvas(4/3*windowHeight, windowHeight, WEBGL);
+	colorMode(HSB, 255);
+  frameRate(60);
 }
 
 function draw() {
-    fill(mouseX/4, mouseY/4, (mouseX+mouseY)/8)
-    circle(mouseX, mouseY, (mouseX+mouseY)/5);
+  
+  background(0);
+  ambientLight(100);
+  pointLight(200, 200, 200, -400, -800, 800);
+  stroke('black');
+  rotateY(-frameCount / 100.0);
+  rotateX(frameCount / 50.0);
+
+
+  for (let i = 0; i < 256; i++) {
+    push();
+    normalMaterial();
+    
+    rotateZ(sin(frameCount)/50 + i / 24.0+1000);
+    rotateY(cos(frameCount)/50 / 22.0);
+    //rotateX(sin(frameCount)/50 + i / 20.0+500);
+    translate(random(500), 0, 0);
+    scale(0.01)
+  
+    
+    box(width / 10);
+    pop();
+  }
+  
+  for (let i = 0; i < 256; i++) {
+    push();
+    normalMaterial();
+    
+    rotateY(cos(frameCount)/50 / 22.0-500);
+    rotateZ(sin(frameCount)/50 + i / 24.0+1000);
+    //rotateX(sin(frameCount)/50 + i / 20.0+500);
+    translate(random(500), 0, 0);
+    scale(0.01)
+  
+    
+    box(width / 10);
+    pop();
+  }
+  
+  for (let i = 0; i < 256; i++) {
+    push();
+    normalMaterial();
+    
+    rotateY(cos(frameCount)/50 / 22.0+500);
+    rotateZ(sin(frameCount)/50 + i / 24.0+1000);
+    //rotateX(sin(frameCount)/50 + i / 20.0+500);
+    translate(random(500), 0, 0);
+    scale(0.01)
+  
+    
+    box(width / 10);
+    pop();
+  }
+  for (let i = 0; i < 256; i++) {
+    push();
+    normalMaterial();
+    
+    rotateY(cos(frameCount)/50 / 22.0+2000);
+    rotateZ(sin(frameCount)/50 + i / 24.0+1000);
+    //rotateX(sin(frameCount)/50 + i / 20.0+500);
+    translate(random(500), 0, 0);
+    scale(0.01)
+  
+    
+    box(width / 10);
+    pop();
+  }
+  for (let i = 0; i < 256; i++) {
+    push();
+    normalMaterial();
+    
+    rotateY(cos(frameCount)/50 / 22.0-2000);
+    rotateZ(sin(frameCount)/50 + i / 24.0+1000);
+    //rotateX(sin(frameCount)/50 + i / 20.0+500);
+    translate(random(500), 0, 0);
+    scale(0.01)
+  
+    
+    box(width / 10);
+    pop();
+  }
 }

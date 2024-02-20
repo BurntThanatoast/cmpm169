23eff17f10ec20c1eb0a7ec7f6bcb09d2e4449e9
Joshua Vaillancourt <jjvailla@ucsc.edu>
2024-02-19 23:31:33 -0800

    attempt 1 for exp 6

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,67 +1,70 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - Experiment 6: Grammars & Text Art
+// Author: Joshua Vaillancourt
+// Date: 2/19/24
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+/**
+ * changing the size and the position of a letter
+ *
+ * MOUSE
+ * position x          : size
+ * position y          : position
+ * drag                : draw
+ *
+ * KEYS
+ * a-z                 : change letter
+ * ctrl                : save png
+ */
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+'use strict';
 
-// Globals
-let myInstance;
-let canvasContainer;
+var font = 'times-new-roman';
+var letter = 'L';
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
     canvas.parent("canvas-container");
     // resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+    //createCanvas(windowWidth, windowHeight);
+    background(0);
+    fill(250);
+    frameRate(60);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    textFont(font);
+    textAlign(CENTER, CENTER);
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    //rotate(sin(frameCount)/20);
+    text(letter, (sin(frameCount) * width/2) + width/2, (cos(frameCount) * height/2) + height/2);
+    text(letter, sin(frameCount) * width, height/2);
+    text(letter, width/2, sin(frameCount) * height);
+    text(letter, sin(frameCount) * width, sin(frameCount) * height);
+    text(letter, width - sin(frameCount) * width, sin(frameCount) * height);
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function mouseMoved() {
+    //clear();
+    background(0,0,0, 100)
+    textSize(abs(mouseX-mouseY) + 1);
+    //text(letter, mouseX, mouseY);
+}
+
+function mouseDragged() {
+    background(0,0,0, 1)
+    textSize(abs(mouseX-mouseY) + 1);
+    text(letter, mouseX, mouseY);
+}
+
+function keyReleased() {
+    if (keyCode == CONTROL) saveCanvas(gd.timestamp(), 'png');
+}
+
+function keyTyped() {
+    letter = key;
 }
